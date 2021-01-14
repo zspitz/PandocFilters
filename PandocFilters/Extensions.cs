b6b2dbbfd7971bbc28f2d5b84a0f002c6f6e095f
Zev Spitz <shivisi@hotmail.com>
2021-01-14 23:26:27 +0200

    ToInlines extension method; disable tests

--- PandocFilters/Extensions.cs
+++ PandocFilters/Extensions.cs
@@ -1,16 +1,23 @@
-﻿using System;
+﻿using PandocFilters.Ast;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PandocFilters {
     public static class Extensions {
         // based on https://stackoverflow.com/a/8728686/111794
-        public static Func<T1, TResult> WrapWith<T1, T2, TResult>(this Func<T1, T2> innerFunc, Func<T2, TResult> outerFunc) =>
+        internal static Func<T1, TResult> WrapWith<T1, T2, TResult>(this Func<T1, T2> innerFunc, Func<T2, TResult> outerFunc) =>
             arg => outerFunc(innerFunc(arg));
 
-        public static Func<T, T> Chain<T>(this IEnumerable<Func<T, T>> src) =>
-            src.Aggregate((running, next) => running.WrapWith(next));
+        public static IEnumerable<Inline> ToInlines(string s) {
+            var firstWord = true;
+            foreach (var word in s.Split(' ')) {
+                if (firstWord) {
+                    firstWord = false;
+                } else {
+                    yield return new Space();
+                }
+                yield return new Str(word);
+            }
+        }
     }
 }
