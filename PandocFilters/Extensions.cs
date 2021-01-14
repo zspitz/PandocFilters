47ac59972eeb5e0ee657d5e085993318237fd635
Zev Spitz <shivisi@hotmail.com>
2021-01-14 23:53:28 +0200

    Remove ToInlines (just create an Str instead)

--- PandocFilters/Extensions.cs
+++ PandocFilters/Extensions.cs
@@ -7,17 +7,5 @@ namespace PandocFilters {
         // based on https://stackoverflow.com/a/8728686/111794
         internal static Func<T1, TResult> WrapWith<T1, T2, TResult>(this Func<T1, T2> innerFunc, Func<T2, TResult> outerFunc) =>
             arg => outerFunc(innerFunc(arg));
-
-        public static IEnumerable<Inline> ToInlines(this string s) {
-            var firstWord = true;
-            foreach (var word in s.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)) {
-                if (firstWord) {
-                    firstWord = false;
-                } else {
-                    yield return new Space();
-                }
-                yield return new Str(word);
-            }
-        }
     }
 }
